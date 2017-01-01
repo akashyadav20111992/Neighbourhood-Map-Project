eecae4b66398b7d006c3f153f4f23b4f35ad16b9
akash yadav <akashyadav20111992@gmail.com>
2017-01-02 01:33:00 +0530

    error handling problem

---
+++ app.js
@@ -0,0 +1,163 @@
+//https://discussions.udacity.com/t/filtering-my-list-of-locations-with-ko/38858
+//https://discussions.udacity.com/t/triggering-marker-bounce-w-list-binding/41089/6
+//https://developers.google.com/maps/documentation/javascript/examples/marker-animations
+//https://developer.mozilla.org/en/docs/Web/API/GlobalEventHandlers/onerror
+var map;
+var locations = [
+  {id : 1 , title: "Home", location: {lat: 28.159818, lng: 76.813626}},
+  {id : 2 , title: "Cinema", location: {lat: 28.191354, lng: 76.813860}},
+  {id : 3 , title: "McDonald's", location: {lat: 28.196682, lng: 76.812181}},
+  {id : 4 , title: "HDFC Bank", location: {lat: 28.205816, lng: 76.808195}},
+  {id : 5 , title: "Modern Public School", location: {lat: 28.208898, lng: 76.808729}},
+];
+// Create a new blank array for all the listing markers.
+var markers = [];
+
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 28.159818, lng: 76.813626},
+    zoom: 14,
+    //mapTypeControl: false
+  });
+
+
+
+  var largeInfowindow = new google.maps.InfoWindow();
+  var bounds = new google.maps.LatLngBounds();
+  // The following group uses the location array to create an array of markers on initialize.
+  for (var i = 0; i < locations.length; i++) {
+    // Get the position from the location array.
+    var position = locations[i].location;
+    var title = locations[i].title;
+    // Create a marker per location, and put into markers array.
+     var marker = new google.maps.Marker({
+      map : map,
+      position : position,
+      title : title,
+      animation : google.maps.Animation.DROP,
+      id : i
+    });
+    // Push the marker to our array of markers.
+    markers.push(marker);
+    bounds.extend(markers[i].position);
+    // Create an onclick event to open an infowindow at each marker.
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+      bounce(this);
+    });
+  }
+  map.fitBounds(bounds);
+  //document.getElementById('show-listings').addEventListener('click', showListings);
+  //document.getElementById('hide-listings').addEventListener('click', hideListings);
+}
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    var abc = [];
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
+    var wikiRequestTimeout = setTimeout(function(){
+      abc.push("failed to get wikipedia resources");
+      infowindow.setContent('<div>' + marker.title + '<br>' + marker.position + '<br>' + abc + '</div>');
+    },8000);
+    $.ajax({
+      url : wikiUrl,
+      dataType : "jsonp",
+      success : function(response){
+        var articleList = response[1];
+        for(var i=0;i<1;i++){
+          articleStr = articleList[i];
+          var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+          abc.push(url);
+          infowindow.setContent('<div>' + marker.title + '<br>' + marker.position + '<br>' + '<a href="' + abc +'">'+ abc + '</a>' + '</div>');
+        }
+        clearTimeout(wikiRequestTimeout);
+      }
+    });
+    //console.log(abc);
+
+
+    infowindow.open(map, marker);
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick', function() {
+      infowindow.marker = null;
+    });
+  }
+}
+function bounce(marker){
+  marker.setAnimation(google.maps.Animation.BOUNCE);
+  setTimeout(function(){
+    marker.setAnimation(null);
+  }, 3000);
+}
+
+// This function will loop through the markers array and display them all.
+/*function showListings() {
+  var bounds = new google.maps.LatLngBounds();
+  // Extend the boundaries of the map for each marker and display the marker
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+    bounds.extend(markers[i].position);
+  }
+  map.fitBounds(bounds);
+}
+
+// This function will loop through the listings and hide them all.
+function hideListings() {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+}*/
+var ViewModel = function(){
+  var self = this;
+  this.listings = ko.observableArray();
+  this.filterBar = ko.observable();
+  for(var i=0;i<locations.length;i++){
+    this.listings.push(locations[i]);
+  }
+  this.search = function(typed){
+    self.listings([]);
+    for(var i=0;i<locations.length;i++){
+      if(locations[i].title.toLowerCase().indexOf(typed.toLowerCase()) >= 0){
+        self.listings.push(locations[i]);
+        markers[i].setVisible(true);
+      }
+      else{
+        markers[i].setVisible(false);
+      }
+    }
+  }
+  this.filterBar.subscribe(self.search);
+  this.listHitMarker = function(){
+    for(var i=0;i<locations.length;i++){
+      if(locations[i].id === this.id){
+        google.maps.event.trigger(markers[i], 'click');
+      }
+    }
+  }
+}
+ko.applyBindings(new ViewModel());
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+    var string = msg.toLowerCase();
+    var substring = "script error";
+    if (string.indexOf(substring) > -1){
+        alert('Script Error: See Browser Console for Detail');
+    } else {
+        var message = [
+            'Message: ' + msg,
+            'URL: ' + url,
+            'Line: ' + lineNo,
+            'Column: ' + columnNo,
+            'Error object: ' + JSON.stringify(error)
+        ].join(' - ');
+
+        alert(message);
+    }
+
+    return false;
+};
